6a5a00880e7bfb64ccb860f680a145c14d5aae7a
Ulises Moreno <ulises.moreno.mendoza@hotmail.com>
2022-07-09 20:22:20 -0500

    Ejercicio 02

---
+++ Curso de Javascript Básico/Ejercicio 02/index.js
@@ -0,0 +1,10 @@
+const libroFav = {
+  titulo: "El señor de los anillos",
+  autor: "J.R.R. Tolkien",
+  fecha: "1954-29-06",
+  amazon: "https://cutt.ly/4Lh3QuH",
+};
+const cumpleañosUlises = new Date("may 9, 1996");
+let ulises = ["Ulises", 25, true, cumpleañosUlises, libroFav];
+
+console.log(ulises);
