7238da308c70707bfde30cd6f92b9bf0bb3714a3
Ulises Moreno <ulises.moreno.mendoza@hotmail.com>
2022-07-13 19:19:18 -0500

    Curso Javascript Ejercicio 05

---
+++ Curso de Javascript Básico/Ejercicio 05/index.js
@@ -0,0 +1,6 @@
+var alturaCm = 185;
+var alturaM = 1.85;
+var pesoKg = 90.5;
+var alturaRedondeado = Math.round(alturaM);
+var pesoRedondeado = Math.floor(pesoKg);
+const maxNum = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
